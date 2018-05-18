63db8583d2b72803dfd91f77e97f51dcdd1b16ad
Marcel Lucas <lucas.marcel@epitech.eu>
2018-05-19 01:04:56 +0200

    add my_str_to_array_42.c

---
+++ src/utils/my_str_to_array_42.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** my_str_to_array_42
+** File description:
+** 42
+*/
+
+#include "42.h"
+
+char **my_str_to_array_42(char *str, char delim)
+{
+	char **tab = NULL;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	return (tab);
+}
