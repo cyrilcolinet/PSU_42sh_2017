e7a4d8a33c3a241be586617c47285e21f3e1ed7e
GRAS Lucas <lucas.gras@hotmail.fr>
2018-05-22 13:47:20 +0200

    [-] Fix catcher

--- src/sig/ctrl_catcher.c
+++ src/sig/ctrl_catcher.c
@@ -7,19 +7,7 @@
 
 #include "42.h"
 
-void	*thread_ctrl(void *arg)
-{
-	while (1) {
-
-	}
-}
-
 void	ctrl_catcher(char sig)
 {
-	pthread_t	ctrl_catcher_thread;
 
-	pthread_create(&ctrl_catcher_thread, NULL, thread_ctrl, NULL);
-	pthread_join(ctrl_catcher_thread, NULL);
-	if (sig && sig == 0x0C)
-		printf("ctrl l !\n");
 }
