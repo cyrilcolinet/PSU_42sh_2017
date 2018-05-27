d638687ba4ed33ecbb088c63812bfacfdaddc460
Adrien Iacono <adrien.iacono@epitech.eu>
2018-05-27 21:34:47 +0200

    update pipes with building

--- src/builtin/call_builtin.c
+++ src/builtin/call_builtin.c
@@ -16,8 +16,17 @@ void call_builtins(int func, char **av, env_t *env, parser_t *parser)
 	if (func == 4)
 		exit_success(env, av, parser);
 	else {
+		dup2(env->pipe_fdin, 0);
+		close(env->pipe_fdin);
+		if (env->pipe_next) {
+			dup2(env->pipe_fd[1], 1);
+			close(env->pipe_fd[1]);
+		}
+		close(env->pipe_fd[0]);
 		if (func > 4)
 			func--;
 		builtins[func](env, av);
+		close(env->pipe_fd[1]);
+		env->pipe_fdin = env->pipe_fd[0];
 	}
 }
